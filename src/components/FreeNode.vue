04db1c9276f0217a9d13341ce4741c1f1798bcf3
Jan Kropiwnicki <jan.kropiwnicki@infotech.edu.pl>
2025-05-16 12:04:38 +0200

    Added free node dragging

--- src/components/FreeNode.vue
+++ src/components/FreeNode.vue
@@ -1,10 +1,14 @@
 <script setup>
-import {computed, onMounted, watch} from "vue";
+import {computed, onMounted, ref, watch} from "vue";
     import {store} from "@/store.js";
 
     const props = defineProps({
         id: Number,
     })
+    const draggable = ref(false);
+    const dragOffsetX = ref(0);
+    const dragOffsetY = ref(0);
+
     const freeNode = computed({
         get: () => store.gates[props.id],
         set: val => {
@@ -16,6 +20,26 @@ import {computed, onMounted, watch} from "vue";
             store.gates[props.id] = val
         }
     })
+
+    function handleMouseDown(ev) {
+        draggable.value = true;
+        dragOffsetX.value = ev.clientX - freeNode.value.x;
+        dragOffsetY.value = ev.clientY - freeNode.value.y;
+        document.addEventListener('mousemove', handleMouseMove)
+        document.addEventListener('mouseup', handleMouseUp)
+    }
+
+    function handleMouseMove(ev) {
+        if (!draggable.value) return
+        freeNode.value.x = ev.clientX - dragOffsetX.value;
+        freeNode.value.y = ev.clientY - dragOffsetY.value;
+    }
+
+    function handleMouseUp() {
+        draggable.value = false;
+        document.removeEventListener('mousemove', handleMouseMove)
+        document.removeEventListener('mouseup', handleMouseUp)
+    }
 
     onMounted(() => {
         freeNode.value = freeNode.value;
@@ -29,14 +53,31 @@ import {computed, onMounted, watch} from "vue";
 </script>
 
 <template>
-    <g :fill="getColor()">
+    <g>
         <circle
-            :r="0 < freeNode.nodes.length < 3 ? 2 : 6"
+            :fill="getColor()"
+            :r="freeNode.nodes.length > 0 && freeNode.nodes.length < 3 ? 2 : 6"
             :cx="freeNode.x" :cy="freeNode.y"
+        />
+        <circle class="drag-circle"
+                :class="{filled: freeNode.nodes.length > 0 && freeNode.nodes.length < 3}"
+                r="10" :cx="freeNode.x" :cy="freeNode.y" @mousedown="handleMouseDown"
         />
     </g>
 </template>
 
 <style scoped>
-
+    .drag-circle {
+        cursor: move;
+        fill: #0000;
+        border-radius: 100%;
+    }
+    .drag-circle:hover {
+        outline: #8884 solid 4px;
+        outline-offset: -4px;
+    }
+    .drag-circle.filled:hover {
+        outline: none;
+        fill: #8884
+    }
 </style>
