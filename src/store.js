7f204bc4799a26094132af9faf27d4df051eaf05
Jan Kropiwnicki <jan.kropiwnicki@infotech.edu.pl>
2025-05-21 11:11:19 +0200

    Cleaned up code

--- src/store.js
+++ src/store.js
@@ -1,42 +1,8 @@
 import { reactive } from "vue";
-import {And, Nand, Or, Not, Connection, FreeNode, Node, Input, Output} from "@/gates.js";
 
 export const store = reactive({
-    gates: [
-        new Input(100, 100),
-        new Nand(300, 100),
-        new FreeNode(250, 150, [
-            new Node(0, 0, false),
-            new Node(0, 0, true),
-            new Node(0, 0, true),
-        ]),
-        new FreeNode(200, 100, [
-            new Node(0, 0, false),
-            new Node(0, 0, true),
-        ]),
-        new Output(500, 100)
-    ],
-    connections: [
-        new Connection([
-            {gate: 0, dest: 'out'},
-            {gate: 3, dest: 0},
-        ]),
-        new Connection([
-            {gate: 3, dest: 1},
-            {gate: 2, dest: 0},
-        ]),
-        new Connection([
-            {gate: 2, dest: 1},
-            {gate: 1, dest: 'a'},
-        ]),
-        new Connection([
-            {gate: 2, dest: 2},
-            {gate: 1, dest: 'b'},
-        ]),
-        new Connection([
-            {gate: 1, dest: 'out'},
-            {gate: 4, dest: 'a'},
-        ])
-    ],
-    newConnectionNodes: []
+    gates: [],
+    connections: [],
+    newConnectionNodes: [],
 })
+
